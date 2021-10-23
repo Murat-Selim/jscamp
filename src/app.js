97b4a7ffe6b84842c25fbb7a512b516bb964bbbe
Murat-Selim <66679051+Murat-Selim@users.noreply.github.com>
2021-10-23 19:36:30 +0300

    18 ekim ders sonu

---
+++ src/app.js
@@ -0,0 +1,14 @@
+console.log("Merhaba Kodlama.io")
+
+let dolarBugun = 9.30
+let dolarDun = 9.20
+
+console.log(dolarDun)
+
+let konutKredileri = ["Konut Kredisi", "Emalk konut Kredileri","Kamu konut Kredileri"]
+
+console.log("<ul>")
+for(let i=0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"<li>")
+}
+console.log("<ul>")
