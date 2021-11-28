abd31c0ec25d7d45c72982d6f9668203857ad89f
Murat-Selim <66679051+Murat-Selim@users.noreply.github.com>
2021-11-28 13:30:05 +0300

    ders 4

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,5 +1,6 @@
 import { BaseLogger, ElasticLogger, MongoLogger } from "../crossCuttingConcerns/logging/logger.js"
 import User from "../models/user.js"
+import Customer from "../models/customer.js"
 import UserService from "../services/userService.js"
 
 console.log("User component yuklendi")
@@ -13,10 +14,18 @@ let user2 = new User(2,"Baran","Gokcekli","Mugla")
 userService.add(user1)
 userService.add(user2)
 
-console.log(userService.list())
-console.log(userService.getById(2))
+//console.log(userService.list())
+//console.log(userService.getById(2))
 
 
-userService.list()
-userService.getById(1)
+console.log("----------------------")
+userService.load()
+userService.add(new Customer(1,"Seda","Yilmaz","Ankara","fhgfghfgf"))
+console.log(userService.customers)
+console.log(userService.employees)
+console.log(userService.errors)
+console.log(userService.getCustomersSorted())
+
+
+
 
